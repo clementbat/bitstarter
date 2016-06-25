8cd43d63f1dcd7484e8065905d4d0300e24fa152
Ubuntu <ubuntu@ip-172-31-27-196.eu-west-1.compute.internal>
2016-06-25 19:23:11 +0000

    test2

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+var content;
+fs.readFileSync('/index.html', function read(err, data) {content = data;});
+
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('/index.html'))
+  response.send(content)
 })
 
 app.listen(app.get('port'), function() {
