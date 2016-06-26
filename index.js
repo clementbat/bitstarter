02e6f763c550087c4ffab7c729df451c0fe9bbff
Ubuntu <ubuntu@ip-172-31-27-196.eu-west-1.compute.internal>
2016-06-26 09:00:27 +0000

    specify that index.html is a string

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 var content = new Buffer(30);
 
-fs.readFile('index.html', (err, data) => {
+fs.readFile('index.html', 'utf8', (err, data) => {
 if (err) throw err;
 console.log(data);
 content.write(data);
