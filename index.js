d8aedf7c5e7f494e73c605e6ac6dbb3ba7cb2900
clementbat <ubuntu@ip-172-31-27-196.eu-west-1.compute.internal>
2016-06-29 17:07:11 +0000

    debug

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 fs.readFile('index.html', 'utf8', (err, data) => {
+console.log(data);
 if (err) throw err;
 content.write(data);
 app.get('/', function(request, response) {
