3fee92e096fda553b8eef9e7293c2a555965ef30
clementbat <ubuntu@ip-172-31-27-196.eu-west-1.compute.internal>
2016-06-29 17:14:00 +0000

    ajout de express

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var content = new Buffer(30000);
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-fs.readFile('index.html', 'utf8',function(err, data) {
+fs.readFile('public/index.html', 'utf8',function(err, data) {
 console.log(data);
 if (err) throw err;
 content.write(data);
