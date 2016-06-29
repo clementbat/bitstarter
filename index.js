8c4258fb504efade1ae67111edb8b68e8dd58ac3
clementbat <ubuntu@ip-172-31-27-196.eu-west-1.compute.internal>
2016-06-29 17:11:06 +0000

    syntax change

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var express = require('express')
 var app = express()
 const fs = require('fs')
@@ -7,7 +8,7 @@ var content = new Buffer(30000);
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-fs.readFile('index.html', 'utf8', (err, data) => {
+fs.readFile('index.html', 'utf8',function(err, data) {
 console.log(data);
 if (err) throw err;
 content.write(data);
